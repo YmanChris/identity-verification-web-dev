a9d2eb7f32843d2e54731d58d59476266e036334
王在 <wangzai@wangzaideMacBook-Pro-3.local>
2026-02-09 15:09:48 +0800

    fix: vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,15 @@ import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production'
-
   return {
-    /**
-     * GitHub Pages 项目站点必须设置 base
-     * 例：https://xxx.github.io/my-app/
-     * 仓库名是 my-app
-     */
-    base: isProd ? '/identity-verification-web-dev/' : '/',
-
+    // ✅ 关键：让产物引用相对路径 ./assets/...
+    base: isProd ? './' : '/',
     server: {
       host: true,
-      // 只在本地开发启用 https
       https: !isProd,
       port: 5173
     },
-
-    plugins: [
-      react(),
-      // mkcert 仅用于本地开发
-      ...(isProd ? [] : [mkcert()])
-    ]
+    plugins: [react(), ...(isProd ? [] : [mkcert()])]
   }
 })
 
